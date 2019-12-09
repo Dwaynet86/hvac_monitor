b8f333a7dd3cee44802498d45db0b734c417d7a2
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2019-12-08 23:31:37 -0600

    Update MQTT_Connect.py

--- MQTT_Connect.py
+++ MQTT_Connect.py
@@ -7,12 +7,26 @@ MQTT_USER = "DEVMQTT"
 MQTT_PASS = "DEVMQTT"
 MQTT_CLIENT = "HVAC-MONITOR"
 
+
+def on_connect(client, userdata, flags, rc):
+  print("Connected with result code "+str(rc))
+  client.subscribe("topic/test")
+
+def on_message(client, userdata, msg):
+  if msg.payload.decode() == "Hello world!":
+    print("Yes!")
+    client.disconnect()
+
+    
 client = mqtt.Client(MQTT_CLIENT)
 client.username_pw_set(MQTT_USER, MQTT_PASS)
 client.connect (MQTT_SERVER, port=MQTT_PORT)
 
+client.on_connect = on_connect
+client.on_message = on_message
 
+client.loop_forever()
 
-client.disconnect()
+#client.disconnect()
 
 
