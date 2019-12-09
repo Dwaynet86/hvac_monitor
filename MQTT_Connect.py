1b9a95348c9c70dfb2deb9822f95143a59189bde
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2019-12-08 23:08:30 -0600

    Update MQTT_Connect.py

--- MQTT_Connect.py
+++ MQTT_Connect.py
@@ -8,7 +8,7 @@ MQTT_PASS = "DEVMQTT"
 MQTT_CLIENT = "HVAC-MONITOR"
 
 client = mqtt.Client(MQTT_CLIENT)
-client = username_pw_set(MQTT_USER, MQTT_PASS)
+client.username_pw_set(MQTT_USER, MQTT_PASS)
 client.connect (MQTT_SERVER, port=MQTT_PORT)
 
 
