91ca46ad86e86dcabd8fc85add250239028f7493
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2019-12-09 21:19:29 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ from mqtt_connect import *
 
 def on_connect(client, userdata, flags, rc):
   print("Connected with result code "+str(rc))
-  print("Subscribing..." + MQTT_PATH)
-  client.subscribe("$SYS/#")
+  #print("Subscribing..." + MQTT_PATH)
+  client.subscribe("#")
 
 def on_message(client, userdata, msg):
   if msg.payload.decode() == "Hello world!":
