abd43a7b1c5f03dada62370340f5f1fa27d88eb1
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2019-12-10 17:36:15 -0600

    Update main.py

--- main.py
+++ main.py
@@ -5,40 +5,8 @@ from mqtt_connect import *
 from time import sleep
 import os
 
-def on_connect(client, userdata, flags, rc):
-  print("Connected with result code "+str(rc))
-  client.subscribe("#")
 
-def on_message(client, userdata, msg):
-  if msg.payload.decode() == "Hello world!":
-    print("Yes!")
-  
-  print(msg.topic + msg.payload)
-
-    
-def on_publish(client, userdata, flags):
-  print("published!")
-
-  
-  
-def on_subscribe(client, userdata, flags):
-  print("subscribed")  
-
-def on_set(client, userdata, msg):
-    # This callback will only be called for messages with topics that match
-    # hvac_monitor/set/#
-    
-    print("MESSAGES: " + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-
-def on_disconnect(client, userdata, msg):
-  if msg.payload.decode() == "Disconnect":
-    
-    client.loop_stop()
-    print("Loop Stopped..")
-    client.disconnect()
-    print("Disconnected...")
-    os._exit(1)
-  
+#setup  
 client = mqtt.Client(MQTT_CLIENT)
 client.username_pw_set(MQTT_USER, MQTT_PASS)
 client.connect (MQTT_SERVER, port=MQTT_PORT)
@@ -50,6 +18,8 @@ client.on_publish = on_publish
 client.on_subscribe = on_subscribe
 
 client.loop_start()
+
+#Main loop
 while True:
   sleep(10)
   client.publish("hassio/hvac_monitor/status/","supply:70",2)
