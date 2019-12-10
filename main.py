c9cd9d09beb98277c8c2c7b88d836549bd7baa1b
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2019-12-09 21:33:52 -0600

    Update main.py

--- main.py
+++ main.py
@@ -11,9 +11,7 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
   if msg.payload.decode() == "Hello world!":
     print("Yes!")
-  if msg.payload.decode() == "Disconnect":
-    print("Disconnecting...")
-    client.disconnect()
+  
   print(msg.topic + msg.payload)
 
     
@@ -30,12 +28,17 @@ def on_set(client, userdata, msg):
     # hvac_monitor/set/#
     
     print("MESSAGES: " + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
-  
+
+def on_disconnect():
+  if msg.payload.decode() == "Disconnect":
+    print("Disconnecting...")
+    client.disconnect()
   
 client = mqtt.Client(MQTT_CLIENT)
 client.username_pw_set(MQTT_USER, MQTT_PASS)
 client.connect (MQTT_SERVER, port=MQTT_PORT)
 client.message_callback_add("hvac_monitor/set/#", on_set)
+client.message_callback_add("hvac_monitor/disconnect", on_disconnect)
 client.on_connect = on_connect
 client.on_message = on_message
 client.on_publish = on_publish
