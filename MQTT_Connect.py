57ec5b58e7a536aa216ca86d0028eebf63dded5d
Dwayne Truex <51888733+Dwaynet86@users.noreply.github.com>
2019-12-09 20:06:50 -0600

    Update MQTT_Connect.py

--- MQTT_Connect.py
+++ MQTT_Connect.py
@@ -15,15 +15,29 @@ def on_connect(client, userdata, flags, rc):
 def on_message(client, userdata, msg):
   if msg.payload.decode() == "Hello world!":
     print("Yes!")
+  if msg.payload.decode() == "Disconnect":
     client.disconnect()
 
     
+def on_publish(client, userdata, flags):
+  print("publish!")
+
+  
+  
+def on_subscribe(client, userdata, flags):
+  print("subscribed")  
+
+
+  
+  
 client = mqtt.Client(MQTT_CLIENT)
 client.username_pw_set(MQTT_USER, MQTT_PASS)
 client.connect (MQTT_SERVER, port=MQTT_PORT)
 
 client.on_connect = on_connect
 client.on_message = on_message
+client.on_publish = on_publish
+client.on_subscribe = on_subscribe
 
 client.loop_forever()
 
